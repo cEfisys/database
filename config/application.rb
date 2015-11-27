ae71a21f176847b5d2abf1f8054e0d8d92577946
haviana <hugohaviana@gmail.com>
2015-11-27 21:11:34 +0000

    add log4r debugger and improved actuators

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,21 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+# add these line for log4r
+require 'log4r'
+require 'log4r/yamlconfigurator'
+require 'log4r/outputter/datefileoutputter'
+include Log4r
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+module Zurich
+  class Application < Rails::Application
+    #...
+    # assign log4r's logger as rails' logger.
+    log4r_config= YAML.load_file(File.join(File.dirname(__FILE__),"log4r.yml"))
+    YamlConfigurator.decode_yaml( log4r_config['log4r_config'] )
+    config.logger = Log4r::Logger[Rails.env]
+  end
+end
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
