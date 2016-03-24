a44f50385c49915967e98b34908f8d997ed6a4ca
haviana <hugohaviana@gmail.com>
2016-03-24 21:11:58 +0000

    UI changed

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,11 +4,11 @@
 //function setBindings(){
 //
 //    $("nav a").click(function(e){
-//        e.preventDefault(); // Prevent tags session from working. Stops the anchors texts
+//       // e.preventDefault(); // Prevent tags session from working. Stops the anchors texts
 //        var sessionID =e.currentTarget.id+ "Section";
 //        $("html body").animate({
 //            scrollTop: $('#' + sessionID).offset().top
 //        },5000)
 //    })
 //}
-$('body').scrollspy({ target: '#navbar-example' })
+$('body').scrollspy({ target: '#navbar-example',duration:1000 })
